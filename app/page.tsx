8055d089b39466e451f04ec3b4aefdba0f0a8cab
Hamidreza Fallahi <h.falahi@ariansystemdp.local>
2025-11-23 14:10:00 +0330

    deploy 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
    <div className="flex justify-center items-center w-full h-screen">
     آناهیتا فهمیده
+    تست دیپلوی
    </div>
   );
 }
